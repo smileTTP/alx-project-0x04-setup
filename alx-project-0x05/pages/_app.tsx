acc16aae4378ecba158d64e0e6e3fc24f9095854
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-28 21:07:28 +0100

    Include a ContextAPI for global state management

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,11 +1,14 @@
 import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { CountProvider } from "@/context/CountContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Layout>
-    <Component {...pageProps}/>
-  </Layout>
-  );
+    <CountProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CountProvider>
+  )
 }
